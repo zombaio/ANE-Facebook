98ba5964285d4d5ef2bcc8c355b43b23b9086a42
Adam Schlesinger <adam@freshplanet.com>
2016-04-19 14:52:24 -0400

    updated build tools/jars

---
+++ android/src/com/adobe/air/FacebookActivityResultCallback.java
@@ -0,0 +1,4 @@
+package com.adobe.air;
+
+public interface FacebookActivityResultCallback extends AndroidActivityWrapper.ActivityResultCallback {
+}
--- android/src/com/freshplanet/ane/AirFacebook/AirFacebookExtension.java
+++ android/src/com/freshplanet/ane/AirFacebook/AirFacebookExtension.java
@@ -1,17 +1,22 @@
 package com.freshplanet.ane.AirFacebook;
 
+import android.content.Intent;
 import android.util.Log;
 
+import com.adobe.air.AndroidActivityWrapper;
+import com.adobe.air.FacebookActivityResultCallback;
 import com.adobe.fre.FREContext;
 import com.adobe.fre.FREExtension;
 
-public class AirFacebookExtension implements FREExtension
+public class AirFacebookExtension implements FREExtension, FacebookActivityResultCallback
 {
-	
+
 	public static String TAG = "AirFacebook";
 	public static Boolean nativeLogEnabled = true;
 	
 	public static AirFacebookExtensionContext context;
+
+    private AndroidActivityWrapper aaw = null;
 
 	public FREContext createContext(String extId)
 	{
@@ -21,9 +26,20 @@ public class AirFacebookExtension implements FREExtension
 	public void dispose()
 	{
 		context = null;
+
+        if (aaw != null) {
+
+            aaw.removeActivityResultListener(this);
+            aaw = null;
+        }
 	}
 	
-	public void initialize() {}
+	public void initialize() {
+
+        log("initialize");
+        aaw = AndroidActivityWrapper.GetAndroidActivityWrapper();
+        aaw.addActivityResultListener(this);
+    }
 	
 	public static void log(String message)
 	{
@@ -51,4 +67,8 @@ public class AirFacebookExtension implements FREExtension
 	{
 		return context != null ? context.getResourceId(name) : 0;
 	}
+
+    public void onActivityResult(int var1, int var2, Intent var3) {
+        log(var1 + " - " + var2 + " - " + var3.toString());
+    }
 }
